1724a400320f36867a411f6c7ac17fa17340c4a2
Fouziya Afreen <42962887+fouziyaafreen@users.noreply.github.com>
2022-02-26 19:55:03 +0530

    Delete src directory

--- src/src/Component/TodoItem.tsx
+++
@@ -1,8 +0,0 @@
-import classes from './TodoItem.module.css'
-
-const TodoItem: React.FC<{text:string; deleteitemHandler:()=>void }>=(props)=>{
-
-    return <li className={classes.item} onClick={props.deleteitemHandler}>{props.text}</li>
-}
-
-export default TodoItem;
